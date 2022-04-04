0a2a61e861f2428f593df246bf4236caafec4204
gwak <jaewoo010207@gmail.com>
2022-04-04 20:15:23 +0900

    auto login

--- login.js
+++ login.js
@@ -1,6 +1,25 @@
 /* 로그인 버튼 클릭 */
+
+const ID = "jaewoo";
+const PW = "123456";
+
 const loginBtn = document.querySelector(".login__input__btn--login");
 const login = () => {
   alert("로그인");
 };
 loginBtn.addEventListener("click", login);
+
+const idInput = document.querySelector(".login__input--id");
+const pwInput = document.querySelector(".login__input--pw");
+
+const onIdChange = (e) => {
+  //console.log(e.value);
+};
+const onPasswordChange = (e) => {};
+
+const isValidUser = () => {
+  // console.log(idInput.value, pwInput.value);
+  if (idInput.value == ID && pwInput.value == PW) {
+    alert("로그인!");
+  }
+};
